7efda39692dcc291c20ee0d94fdadc88f3b9b2a3
s00190029 <mrdaylight@protonmail.com>
2020-12-08 00:30:05 +0000

    paste working from labsheet

--- app/src/main/java/edu/rdonoghue/simongame/MainActivity.java
+++ app/src/main/java/edu/rdonoghue/simongame/MainActivity.java
@@ -3,6 +3,9 @@ package edu.rdonoghue.simongame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        db.emptyHiScores();     // empty table if required
+
+        // Inserting hi scores
+        Log.i("Insert: ", "Inserting ..");
+        db.addHiScore(new HiScore("20 OCT 2020", "Frodo", 12));
+        db.addHiScore(new HiScore("28 OCT 2020", "Dobby", 16));
+        db.addHiScore(new HiScore("20 NOV 2020", "DarthV", 20));
+        db.addHiScore(new HiScore("20 NOV 2020", "Bob", 18));
+        db.addHiScore(new HiScore("22 NOV 2020", "Gemma", 22));
+        db.addHiScore(new HiScore("30 NOV 2020", "Joe", 30));
+        db.addHiScore(new HiScore("01 DEC 2020", "DarthV", 22));
+        db.addHiScore(new HiScore("02 DEC 2020", "Gandalf", 132));
+
+
+        // Reading all scores
+        Log.i("Reading: ", "Reading all scores..");
+        List<HiScore> hiScores = db.getAllHiScores();
+
+
+        for (HiScore hs : hiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
+
+        Log.i("divider", "====================");
+
+        HiScore singleScore = db.getHiScore(5);
+        Log.i("High Score 5 is by ", singleScore.getPlayer_name() + " with a score of " +
+                singleScore.getScore());
+
+        Log.i("divider", "====================");
+
+        // Calling SQL statement
+        List<HiScore> top5HiScores = db.getTopFiveScores();
+
+        for (HiScore hs : top5HiScores) {
+            String log =
+                    "Id: " + hs.getScore_id() +
+                            ", Date: " + hs.getGame_date() +
+                            " , Player: " + hs.getPlayer_name() +
+                            " , Score: " + hs.getScore();
+
+            // Writing HiScore to log
+            Log.i("Score: ", log);
+        }
     }
 }
