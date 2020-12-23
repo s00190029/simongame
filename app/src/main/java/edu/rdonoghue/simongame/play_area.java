12a4be3ce569a1e0372cec91d9950d241b2d5249
s00190029 <mrdaylight@protonmail.com>
2020-12-23 14:31:57 +0000

    play area

--- app/src/main/java/edu/rdonoghue/simongame/play_area.java
+++ app/src/main/java/edu/rdonoghue/simongame/play_area.java
@@ -10,5 +10,8 @@ public class play_area extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_area);
+
+        
+
     }
 }
